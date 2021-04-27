be9f56643fb518a5f89433661e6daaf8aed882ce
Durawa Michał <michal.durawa@streamsoft.pl>
2021-04-27 09:49:23 +0200

    refactor

--- src/main/java/api/NbpApi.java
+++ src/main/java/api/NbpApi.java
@@ -8,8 +8,11 @@ import demo.CurrencyCode;
 
 public class NbpApi implements Api{
 	private final NbpConfig config = new NbpConfig();
+	private final ExchangeRateProvider provider;
 	
-	public NbpApi() { }
+	public NbpApi() { 
+		provider = config.getExchangeProvider();
+	}
 
 	@Override
 	public Optional<BigDecimal> getRateByCode(CurrencyCode code,
@@ -21,7 +24,7 @@ public class NbpApi implements Api{
 	public Optional<BigDecimal> getRateByCodeAndDate(CurrencyCode code,
 			LocalDate date, BigDecimal amount) {
 		Optional<BigDecimal> rate = 
-				config.getExchangeProvider().getRateByCodeAndDate(code, date);
+				provider.getRateByCodeAndDate(code, date);
 		if (rate.isPresent()) {
 			return Optional.of(rate.get().multiply(amount));
 		}
